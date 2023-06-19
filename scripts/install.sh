40d09dc77b8d4c7d87804e62dcaae15d9558d35e
Zhaoyu Li <zhaoyu@Zhaoyus-MacBook-Pro.local>
2023-06-19 21:41:21 +0800

    update

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,5 +1,6 @@
 pip install torch==1.13.1
 pip install torch-geometric==2.2.0 torch-scatter==2.1.0 torch-sparse==0.6.16
 pip install networkx==3.1 python-sat==0.1.7.dev26
+pip install -r requirements.txt
 
 python setup.py install
--- setup.py
+++ setup.py
@@ -1,13 +1,10 @@
 from setuptools import setup, find_packages
 
-with open('requirements.txt') as f:
-    reqs = f.read()
 
 setup(
     name='g4satbench',
     version='1.0.0',
     description='A Comprehensive Benchmark on GNNs for SAT Solving',
     packages=find_packages(),
-    install_requires=reqs.strip().split('\n'),
     include_package_data=True,
 )
